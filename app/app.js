8a5bd44243753b96bcc2f8ab2735e726e348b6a4
Blake Tsuzaki <dtsuzaki@scu.edu>
2016-10-14 03:50:12 -0700

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,26 @@
+var judgesFile = "json/judges.json";
+
+function searchJudges(identifier, callback) {
+    readJudges(function(judges) {
+        var result = judges.filter(function(judge) {
+            return judge.auth_id === identifier;
+        });
+        if (result.length > 0) {
+            callback(result[0]);
+        } else {
+            callback(null);
+        }
+    });
+}
+
+function readJudges(callback) {
+    var file = new XMLHttpRequest();
+    file.overrideMimeType("application/json");
+    file.open("GET", judgesFile, true);
+    file.onreadystatechange = function () {
+        if (file.readyState === 4 && file.status == "200" && file.responseText) {
+            callback(JSON.parse(file.responseText))
+        }
+    };
+    file.send(null);
+}
