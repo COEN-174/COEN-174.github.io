457d01703f45b29877c78451755ac19b2da95de9
Blake Tsuzaki <dtsuzaki@scu.edu>
2016-10-15 12:56:40 -0700

    Added logic for login and initial project population

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 var judgesFile = "json/judges.json";
-var judgeIdentifierLength = 4;
+var projectsFile = "json/projects.json";
+var sessionsFile = "json/sessions.json";
+var judgeAuthIdentifierLength = 4;
+var judgeIdentifierLength = 3;
+var sessionIdentifierLength = 3;
+var projectIdentifierLength = 3;
 
-function searchJudges(identifier, callback) {
-    if (identifier.length != judgeIdentifierLength) {
-        callback(null);
-    }
-    readJudges(function(judges) {
+function searchJudgesAuth(identifier, callback) {
+    if (identifier.length != judgeAuthIdentifierLength) { callback(null); }
+    readFile(judgesFile, function(judges) {
         var result = judges.filter(function(judge) {
             return judge.auth_id === identifier;
         });
@@ -17,10 +20,52 @@ function searchJudges(identifier, callback) {
     });
 }
 
-function readJudges(callback) {
+function searchJudges(identifier, callback) {
+    if (identifier.length != judgeIdentifierLength) { callback(null); }
+    readFile(judgesFile, function(judges) {
+        var result = judges.filter(function(judge) {
+            return judge.id === identifier;
+        });
+        if (result.length > 0) {
+            callback(result[0]);
+        } else {
+            callback(null);
+        }
+    });
+}
+
+function searchProjects(identifier, callback) {
+    if (identifier.length != projectIdentifierLength) { callback(null); }
+    readFile(projectsFile, function(projects) {
+        var result = projects.filter(function(project) {
+            return project.id === identifier;
+        });
+        if (result.length > 0) {
+            callback(result[0]);
+        } else {
+            callback(null);
+        }
+    });
+}
+
+function searchSessions(identifier, callback) {
+    if (identifier.length != sessionIdentifierLength) { callback(null); }
+    readFile(sessionsFile, function(sessions) {
+        var result = sessions.filter(function(session) {
+            return session.id === identifier;
+        });
+        if (result.length > 0) {
+            callback(result[0]);
+        } else {
+            callback(null);
+        }
+    });
+}
+
+function readFile(filename, callback) {
     var file = new XMLHttpRequest();
     file.overrideMimeType("application/json");
-    file.open("GET", judgesFile, true);
+    file.open("GET", filename, true);
     file.onreadystatechange = function () {
         if (file.readyState === 4 && file.status == "200" && file.responseText) {
             callback(JSON.parse(file.responseText))
