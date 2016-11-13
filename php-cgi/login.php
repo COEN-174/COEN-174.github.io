1eb70614f74b82082493f05e5e47b6b240937bc2
Patrick Miller <p1miller@scu.edu>
2016-11-13 00:59:31 -0800

    New login authorization and users file structure

--- php-cgi/login.php
+++ php-cgi/login.php
@@ -1,53 +1,49 @@
 <?php
+/*
+    This login script checks inputted credentials
+    against list of users and assigns a temporary
+    hex ID used for validating POSTs
+*/
 
-// This login script expects
-// {"username": "nifty username", "password": "hard2guess"}
-// and will check these values against a user/pass file.
-// This script returns HTTP code 200 + credentials on success and 403 if not
+// Get list of saved users
+$users_file = file_get_contents('./users');
+$users = json_decode($users_file, true);
 
+// Get username & password
+$form_login = file_get_contents("php://input");
+$form_login = json_decode($form_login,true);
+$form_username = $form_login["username"];
+$form_password = $form_login["password"];
 
-    //Get associative array of all users and their attributes
-    //Example user file:
-    // {"id":"sballmer","auth_id":"developers","type":["judge"],"name":"Steve Ballmer"},
-    // {"id":"datkinson","auth_id":"asdf","type":["judge","advisor"],"name":"Darren Atkinson"},
-    // {"id":"rdavis","auth_id":"qwerty","type":["advisor"],"name":"Ruth Davis"},
-    // {"id":"shane","auth_id":"theman","type":["admin"],"name":"Shane Wibeto"}
-    $users_file = file_get_contents("users");
-    $users = json_decode($users_file, true);
-
-    $this_user = [];
-
-    //Get login POST data
-    $form_login = file_get_contents("php://input");
-    $form_login = json_decode($data, true);
-    if (!array_key_exists("username", $form_login) or !array_key_exists("password", $form_login)) {
-        http_response_code(400);
-        echo "Bad response";
-        exit("Malformed POST");
-    }
-    $form_username = $login["username"];
-    $form_password = $login["password"];
-
-    //Check POST'd user->pass against our user->password
-    $found = False;
-    for($i = 0; $i < count($users); $i++) {
-        $user = (Array)$users[$i];
-        if ($form_username == $user["id"]) {
-            if ($form_password == $user["auth_id"]) {
-                $found = True;
-                $this_user = $user;
-                unset($this_user["auth_id"]); // Don't ever return password!
-                break;
+// Check if username & password are valid
+$this_user = [];
+$found = False;
+for($i = 0; $i < count($users); $i++) {
+    $user = (Array)$users[$i]; //Treat each index in the array as an array(json obj)
+    if ($form_username == $user["username"]) {
+        if ($form_password == $user["password"]) {
+            $this_user = $user; // Copy user credentials from file
+            unset($this_user["password"]); // Never return passwords
+            $auths_dir = "auths/";
+            // Check if there is an auths directory
+            if (!(file_exists($auths_dir))) {
+                $oldmask = umask(0);
+                mkdir($auths_dir, 0777, true);
+                umask($oldmask);
             }
+            $fp = fopen($auths_dir . $this_user["username"], "w");
+            // This person's auth
+            $auth = bin4hex(random_bytes(5));
+            fwrite($fp, (string)$auth);
+            $this_user["rest_auth"] = $auth;
+            // Return user's credentials
+            echo json_encode($this_user);
+            fclose($fp);
+            $found = True;
+            break;
         }
     }
+}
 
-    if ($found) {
-        http_response_code(200);
-        echo json_encode($this_user); // Return user
-    }
-    else {
-        http_response_code(403);
-        echo "BUZZER WRONG ANSWER";
-    }
+$found ? http_response_code(200) : http_response_code(403);
 ?>
